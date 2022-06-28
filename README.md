613c2a5f1867752bf4f50dff3ed3a04d134f75fb
kona <kitami.kitano20@gmail.com>
2022-06-28 23:38:42 +0900

    :bug: Section3の途中

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ https://www.udemy.com/course/go-blockchain/
 ブロックチェーンで利用するサーバは誰が用意するか？
   個人がサーバを提供している。参加者はMiningを行い報酬を受け取ることができる。
 
+### セクション3
+
+Blockに含むもの
+
+- prev hash
+  - 前のBlockの情報を用いて作成したハッシュ
+- timestamp
+  - 生成時間
+- nonce
+  - ルールに基づいた解答？
+- transactions
+  - 取引時の情報
+  - 実際はPoolと呼ばれる領域にtransactionを記録し、マイニングが終わればtransactionsに書き込む
+
--- blockchain.go
+++ blockchain.go
@@ -3,13 +3,64 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
+	"time"
 )
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp	%d\n", b.timestamp)
+	fmt.Printf("previousHash	%s\n", b.previousHash)
+	fmt.Printf("nonce	%d\n", b.nonce)
+	fmt.Printf("transactions	%s\n", b.transactions)
+}
+
+type BlockChain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockChain() *BlockChain {
+	bc := new(BlockChain)
+	bc.CreatBlock(0, "Init hash")
+	return bc
+}
+
+func (bc *BlockChain) CreatBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func (bc *BlockChain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
 
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
-	log.Println("test")
-	fmt.Println("hello")
+	blockChain := NewBlockChain()
+	blockChain.Print()
+	blockChain.CreatBlock(5, "hash 1")
+	blockChain.Print()
 }
